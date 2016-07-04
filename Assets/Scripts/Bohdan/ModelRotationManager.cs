7de0fac42ec37465b4d8415ffee437a822e4a090
ViktorDev <viktor.marchuk2205@gmail.com>
2016-07-04 18:22:15 +0300

    Added new model, new Scene and pinch object functional.

--- Assets/Scripts/Bohdan/ModelRotationManager.cs
+++ Assets/Scripts/Bohdan/ModelRotationManager.cs
@@ -5,35 +5,28 @@ using UnityEngine.UI;
 public class ModelRotationManager : MonoBehaviour
 {
     [SerializeField] private GameObject _interactModel;
-    [SerializeField] private Camera _ARCamera;
-    [SerializeField, Range(0, 20), Space] private float _rotationRate;
 
-    public Text _text;
-
+    public float speed;
+    public float xDeg, lerpSpeed, friction;
+    Quaternion fromRotation, toRotation;
+    
     private void Update()
     {
         RotateModel();
     }
 
-    public float speed;
-    public float xDeg, lerpSpeed, friction;
-    Quaternion fromRotation, toRotation;
-
     private void RotateModel()
     {
-        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
             RotateTransform(Input.GetTouch(0).deltaPosition.x);
         else RotateTransform(0f);
-        //if(Input.GetMouseButton(0)) 
-        //    RotateTransform(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); 
-        //else RotateTransform(0f, 0f); 
     }
 
-    void RotateTransform(float xNum)
+    private void RotateTransform(float xNum)
     {
-        xDeg -= xNum * 2.0f * friction;
+        xDeg -= xNum * speed * friction;
         fromRotation = _interactModel.transform.rotation;
         toRotation = Quaternion.Euler(0, -xDeg, 0);
-        _interactModel.transform.rotation = Quaternion.Lerp(fromRotation, toRotation, 1);
+        _interactModel.transform.rotation = Quaternion.Lerp(fromRotation, toRotation, lerpSpeed);
     }
 }
--- Assets/Scripts/Bohdan/PinchManager.cs
+++ Assets/Scripts/Bohdan/PinchManager.cs
@@ -1,51 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PinchManager : MonoBehaviour
 {
     [SerializeField] private GameObject _targetObject;
-    
-    private float _scaleFactor = 0.5f; // The rate of change of the scale
-    private float _minScale = 0.6f;
-    private float _maxScale = 2.0f;
+
+    private int _scaleCounter;
 
     private void Update()
     {
-       PinchObject();
+       PinchObject(_targetObject, _scaleCounter);
     }
     
-    private void PinchObject()
+    private void PinchObject(GameObject targetObject, int scaleCounter)
     {
-        // If there are two touches on the device...
-        if (Input.touchCount == 2)
+        Touch touchZero = Input.GetTouch(0);
+        Touch touchOne = Input.GetTouch(1);
+        if ((touchZero.phase == TouchPhase.Moved | touchOne.phase == TouchPhase.Moved))
         {
-            // Store both touches.
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
-
-            // Find the position in the previous frame of each touch.
             Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
             Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
-            // Find the magnitude of the vector (the distance) between the touches in each frame.
             float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
             float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-            // Find the difference in the distances between each frame.
             float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
-            // Change the scale based on the change in distance between the touches.
-            var localTemp = gameObject.transform.localScale;
-            localTemp.x += deltaMagnitudeDiff * _scaleFactor;
-            localTemp.y += deltaMagnitudeDiff * _scaleFactor;
-            localTemp.z += deltaMagnitudeDiff * _scaleFactor;
-
-            // Clamp the scale factor to make sure it's between _minScale and _maxScale.
-            localTemp.x = Mathf.Clamp(localTemp.x, _maxScale, _minScale);
-            localTemp.y = Mathf.Clamp(localTemp.y, _maxScale, _minScale);
-            localTemp.z = Mathf.Clamp(localTemp.z, _maxScale, _minScale);
-
-            _targetObject.transform.localScale = localTemp;
+            if (scaleCounter % 2 == 0)
+            {
+                targetObject.transform.localScale = new Vector3(targetObject.transform.localScale.x - deltaMagnitudeDiff * 0.02f, targetObject.transform.localScale.y - deltaMagnitudeDiff * 0.02f, targetObject.transform.localScale.z - deltaMagnitudeDiff * 0.02f);
+                if (targetObject.transform.localScale.x < 0.1f | targetObject.transform.localScale.y < 0.1f | targetObject.transform.localScale.z < 0.1f) targetObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
+            scaleCounter++;
         }
     }
 }
