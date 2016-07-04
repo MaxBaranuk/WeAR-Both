89f0aad6823cf23c4b16f1d0f4a15594e8aa8613
ViktorDev <viktor.marchuk2205@gmail.com>
2016-07-04 18:51:53 +0300

    Added scene manager.

--- Assets/Scripts/Bohdan/SceneLoader.cs
+++ Assets/Scripts/Bohdan/SceneLoader.cs
@@ -1,15 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private static SceneLoader _instance;
+
+    public static SceneLoader Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<SceneLoader>();
+            }
+            return _instance;
+        }
+    }
+
+    [SerializeField] private string DetailPlanSceneName;
+    [SerializeField] private bool IsAutoActivateSceneWhenLoad;
+
+    /// <summary>
+    /// Load the main scene.
+    /// </summary>
+    public void LoadDetailedPlanScene()
+    {
+        StartCoroutine(AsyncLoadScene(DetailPlanSceneName));
+    }
+
+    /// <summary>
+    /// Async load scene.
+    /// </summary>
+    /// <returns>The load scene.</returns>
+    /// <param name="scene">Scene.</param>
+    IEnumerator AsyncLoadScene(string scene)
+    {
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            // Debug.Log ("Loading progress: " + (progress * 100) + "%");
+
+            // Loading completed
+            if (ao.progress == 0.9f)
+            {
+                // Debug.Log ("Press a key to start");
+
+                if (!IsAutoActivateSceneWhenLoad)
+                {
+                    if (Input.anyKey)
+                    {
+                        ao.allowSceneActivation = true;
+                    }
+                }
+                else
+                {
+                    ao.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
 }
+
+
